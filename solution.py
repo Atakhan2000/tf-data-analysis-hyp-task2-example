bdb136e85dbf9ac0ffd5b1d9dd9af438bfd08374
Atakhan2000 <57829588+Atakhan2000@users.noreply.github.com>
2023-04-13 17:10:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,19 @@
+from scipy.stats import anderson_ksamp
+from hyppo.ksample import MMD
+from scipy.stats import cramervonmises_2samp
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
-from scipy import stats
 
 chat_id = 653318045 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value = anderson_ksamp([x, y]).pvalue 
-    #p_value = stats.ks_2samp(x, y)[1]
-    #p_value = stats.cramervonmises_2samp(x, y).pvalue
     alpha = 0.01
-    return p_value < alpha
+    
+    model = MMD(
+        compute_kernel='laplacian', bias='True'
+    )
+    _, p_value = model.test(x=x.ravel(), 
+               y=y.ravel(), 
+               random_state=26)
+    
+    return p_value < alpha # Ваш ответ, True или False
